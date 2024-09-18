3443ef865c339b21628dc04901647dfa82a7120e
kajmaks <kajmaks123@gmail.com>
2024-09-18 19:10:21 +0200

    params added

--- cos.js
+++ cos.js
@@ -1,9 +1,10 @@
 const http = require('http');
-const https = require('https');
 const fs = require('fs');
+const url = require('url');
 
 const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/html');
+    
     if (req.url === '/') {
         res.end('Strona glowna');
     } else if (req.url === '/json') {
@@ -34,12 +35,24 @@ const server = http.createServer((req, res) => {
                 res.end(data);
             }
         });
-    } else if (req.url === '/get_params') {
-        /*
-            Dla ścieżki /get_params wyświetl w konsoli Node.js parametry przekazane metodą GET.
-            Parametry GET skonwertuj do tablicy i zapisz w pliku params_{timestamp}.json, gdzie timestamp to timestamp przyjęcia danych. Format danych to JSON.
-            Zwróć ze ścieżki dane JSON o treści {'ok': 'ok'}
-        */
+    } else if (req.url.startsWith('/get_params')) {
+        res.setHeader('Content-Type', 'application/json');
+
+        const q = url.parse(req.url, true).query;
+        console.log('GET parameters:', q);
+
+        const ob = Object.entries(q);
+
+        const timestamp = Date.now();
+
+        const file = `params_${timestamp}.json`;
+        fs.writeFile(file, JSON.stringify(ob, null, 2), (err) => {
+        if (err) {
+            console.error(err); 
+        }
+        });
+
+        res.end(JSON.stringify(ob));
+
     }
 }).listen(3000);
-
