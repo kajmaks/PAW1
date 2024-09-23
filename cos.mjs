670a67c8599af7064e5d6fa2d68509a08b96c290
kajmaks <kajmaks123@gmail.com>
2024-09-23 14:47:41 +0200

    zad3

--- cos.mjs
+++ cos.mjs
@@ -1,10 +1,14 @@
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
+import http from 'http';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import mime from 'mime';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/html');
-    
+
     if (req.url === '/') {
         res.end('Strona glowna');
     } else if (req.url === '/json') {
@@ -27,7 +31,7 @@ const server = http.createServer((req, res) => {
             </html>
         `);
     } else if (req.url === '/htmlfile') {
-        fs.readFile('cos.html', (err, data) => {
+        fs.readFile(`${__dirname}/cos.html`, (err, data) => {
             if (err) {
                 res.end('error');
             } else {
@@ -38,21 +42,38 @@ const server = http.createServer((req, res) => {
     } else if (req.url.startsWith('/get_params')) {
         res.setHeader('Content-Type', 'application/json');
 
-        const q = url.parse(req.url, true).query;
+        const q = new URL(req.url, `http://${req.headers.host}`).searchParams;
         console.log('GET parameters:', q);
 
-        const ob = Object.entries(q);
+        const ob = [...q.entries()];
 
         const timestamp = Date.now();
 
         const file = `params_${timestamp}.json`;
         fs.writeFile(file, JSON.stringify(ob, null, 2), (err) => {
-        if (err) {
-            console.error(err); 
-        }
+            if (err) {
+                console.error(err);
+            }
         });
 
         res.end(JSON.stringify(ob));
+    } else {
+        fs.readFile(`${__dirname}/assets/${req.url}`, (err, data) => {
+            if (err) {
+                res.setHeader('Content-Type', 'application/json');
+                res.statusCode = 404;
+                res.write(JSON.stringify({ error: 404}));
+                res.end();
+            } else {
+                let type = mime.getType(req.url);
+                res.setHeader('Content-Type', type);
+                if (type === 'application/json'){
+                    res.end(JSON.stringify(data));
+                } else {
+                    res.end(data);
+                }
+            }
+        });
 
     }
 }).listen(3000);
