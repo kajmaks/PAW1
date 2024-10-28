1978bbad6ebe5cca4ec2126d549065000f7e6fed
kajmaks <kajmaks123@gmail.com>
2024-10-28 14:36:58 +0100

    something

--- routes/kontakt.js
+++ routes/kontakt.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../src/db'); // Import the MySQL connection
+const db = require('../src/db');
 
 router.get('/', (req, res) => {
     res.send(`
